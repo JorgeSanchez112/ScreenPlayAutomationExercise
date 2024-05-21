ddfce1ee4bb179517e94baded362dafcae506d46
Jorge Sanchez <halo3ratons@gmail.com>
2024-05-20 19:12:16 -0500

    DOCS: Targets created in HomePage and LoginPage

--- src/main/java/uiScreens/HomePage.java
+++ src/main/java/uiScreens/HomePage.java
@@ -1,4 +1,7 @@
 package uiScreens;
 
+import net.serenitybdd.screenplay.targets.Target;
+
 public class HomePage {
+    public Target headerMenu = Target.the("menu elements").locatedBy(".navbar-nav > li");
 }
